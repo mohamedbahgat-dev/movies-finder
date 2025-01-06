c2766ece85a6bb4ab2296b3feda398cce4a3e1d7
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-01-06 21:54:40 +0300

    modify footer logo

--- movies-finder-app/src/components/Footer/Footer.jsx
+++ movies-finder-app/src/components/Footer/Footer.jsx
@@ -4,9 +4,8 @@ function Footer() {
   return (
     <div className='flex flex-col h-[100px] backdrop-blur-sm bg-[#001f3f]'>
         <div className='flex justify-between items-center pt-5 mx-24'>
-            <div className='ml-14 rounded-xl bg-gradient-to-r from-green-200 to-cyan-500'>
-                <img className='w-24'
-                     src='../src/assets/logomania.png'/>
+            <div className='ml-14 rounded-xl text-white mx-3 bg-gradient-to-r from-green-200 to-cyan-500'>
+                <div className='mx-3'>Movie<span className='text-sm'>Mania</span></div> 
             </div>
             
             <div className='flex flex-col justify-center items-center text-gray-400'>
